74ee041481c2acfa2b0dd4c4e6297322edee7717
bombermon <npototun0@gmail.com>
2020-12-05 19:10:07 +0300

    added

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from fake_useragent import UserAgent
 import wikipedia
 
+'''
+Берем значит ссылку и через семантик веб анализируем человек это или не человек имя это или не имя 
+'''
+
 def page_open_tbody(name):
     resp = req.get(name)
     soup = BeautifulSoup(resp.text, 'lxml')
@@ -17,33 +21,38 @@ def page_open_body(name):
     main_elem = str(soup.body)
     return main_elem
 
-main_elem = page_open_tbody("https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States")
+wd_url = []
 
+
+main_elem = page_open_body("https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States")
 pattern = re.compile(r'href="/wiki/(.*?)"')
 searcher = re.findall(pattern, main_elem)
+
+new_searcher = []
 for i in searcher:
-    match = re.search(r'[.]', i)
-    if match:
-        searcher.remove(i)
-print(searcher)
+    match = re.search(r'[\.:]', i)
+    if not match:
+        new_searcher.append(i)
 
-new_url = "https://en.wikipedia.org/wiki/" + searcher[0]
+href_list = new_searcher
+print(len(href_list))
+n = 0
+for j in href_list:
+    n += 1
+    try:
+        new_url = "https://en.wikipedia.org/wiki/" + j
+        main_elem = page_open_body(new_url)
 
-
-main_elem = page_open_body(new_url)
-
-pattern = re.compile(r'www\.wikidata\.org/wiki/Special:EntityPage/(.*?)"')
-searcher = re.findall(pattern, main_elem)
-for i in searcher:
-    match = re.search(r'[.]', i)
-    if match:
-        searcher.remove(i)
-
-wd_url = searcher[0]
+        pattern = re.compile(r'www\.wikidata\.org/wiki/Special:EntityPage/(.*?)"')
+        searcher = re.findall(pattern, main_elem)
+        wd_url.append(searcher[0])
+        print('страница %s закрыта' % n)
+    except IndexError:
+        print('не вышло..')
 print(wd_url)
 
 
-#print(soup.tbody)
+
 
 
 '''
