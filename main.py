6079067fa7ab5ab09f5608f71e0c7d928a54484c
bombermon <npototun0@gmail.com>
2020-12-06 21:48:29 +0300

    added

--- main.py
+++ main.py
@@ -9,20 +9,107 @@ import wikipedia
 Берем значит ссылку и через семантик веб анализируем человек это или не человек имя это или не имя 
 '''
 
-def page_open_tbody(name):
-    resp = req.get(name)
-    soup = BeautifulSoup(resp.text, 'lxml')
-    main_elem = str(soup.tbody)
-    return main_elem
+
+def get_wiki_url(wikidata_id, lang='en', debug=False):
+    import requests
+    from requests import utils
+
+    url = (
+        'https://www.wikidata.org/w/api.php'
+        '?action=wbgetentities'
+        '&props=sitelinks/urls'
+        f'&ids={wikidata_id}'
+        '&format=json')
+    json_response = requests.get(url).json()
+    if debug: print(wikidata_id, url, json_response)
+
+    entities = json_response.get('entities')
+    if entities:
+        entity = entities.get(wikidata_id)
+        if entity:
+            sitelinks = entity.get('sitelinks')
+            if sitelinks:
+                if lang:
+                    # filter only the specified language
+                    sitelink = sitelinks.get(f'{lang}wiki')
+                    if sitelink:
+                        wiki_url = sitelink.get('url')
+                        if wiki_url:
+                            return requests.utils.unquote(wiki_url)
+                else:
+                    # return all of the urls
+                    wiki_urls = {}
+                    for key, sitelink in sitelinks.items():
+                        wiki_url = sitelink.get('url')
+                        if wiki_url:
+                            wiki_urls[key] = requests.utils.unquote(wiki_url)
+                    return wiki_urls
+    return None
+
 
 def page_open_body(name):
     resp = req.get(name)
     soup = BeautifulSoup(resp.text, 'lxml')
-    main_elem = str(soup.body)
-    return main_elem
+    main_part = str(soup.body)
+    return main_part
+
+
+# ФУНКЦИЯ ПОЛУЧЕНИЯ ДАТЫ НАЧАЛО
+def get_dates_from_url(url):
+    def get_dates(page, pattern):
+        new = re.findall(pattern, str(page))
+
+        date = ''
+        date_list = []
+        state = False
+        for j in new:
+            for i in j:
+                if i == '<':
+                    state = True
+                if i == '>':
+                    state = False
+                if i == '<':
+                    state = True
+                if i == '>':
+                    state = False
+                if i == '[':
+                    state = True
+                if i == ']':
+                    state = False
+                if not state:
+                    if i != ']' and i != '>':
+                        date += i
+                        temp = i
+            date_list.append(date)
+            date = ''
+
+        data_list_new = []
+
+        for i in date_list:
+            new_elem = i.replace(u'\xa0', u' ')
+            data_list_new.append(new_elem)
+
+        return data_list_new
+
+    changer = page_open_body(url)
+
+    soup = BeautifulSoup(changer)
+    page = soup.find('table', {'class': 'infobox vcard'})
+
+    data_pattern = re.compile(r'<tr>.*?office(.*?)</tr><tr>')
+
+    data_list = get_dates(page, data_pattern)
+
+    if not data_list:
+        data_pattern = re.compile(r'<tr>.*?eign(.*?)</tr><tr>')
+        data_list = get_dates(page, data_pattern)
+
+    return data_list
+
+
+# ФУНКЦИЯ ПОЛУЧЕНИЯ ДАТЫ КОНЕЦ
 
 wd_url = []
-
 
 main_elem = page_open_body("https://en.wikipedia.org/wiki/List_of_state_leaders_in_1900")
 pattern = re.compile(r'href="/wiki/(.*?)"')
@@ -48,9 +135,9 @@ for j in href_list:
         wd_url.append(searcher[0])
         print('страница %s закрыта' % n)
     except IndexError:
+        None
         print('не вышло..')
 print(wd_url)
-
 
 headers_dict = {}
 names = []
@@ -67,7 +154,6 @@ for i in wd_url:
         humanity = results['results']['bindings'][0]['inception']['value']
 
         if humanity == 'http://www.wikidata.org/entity/Q5':
-            print('УРА МЕШОК С КОСТЯМИ КОЖАНЫЙ %s' % i)
             names.append(i)
             sparql = SPARQLWrapper("http://query.wikidata.org/sparql", agent=UserAgent().random)
             sparql.setQuery("""
@@ -96,17 +182,14 @@ for i in wd_url:
                 main_elem = results['results']['bindings'][j]['item']['value']
                 result = re.split('/', main_elem)[-1]
                 headers_dict[i].append(result)
-        else:
-            print('ЭТО НЕ ХУЙНЯ НО И НЕ МЕШОК %s' % i)
 
     except IndexError:
-        print('Это хуйня ' + i)
-
-
-
+        None
 
 print(headers_dict)
 
+for i in headers_dict:
+    url = get_wiki_url(i)
+    ans = get_dates_from_url(url)
 
-
-
+print(ans)
--- test 2.py
+++ test 2.py
@@ -4,73 +4,67 @@ import re
 
 import datefinder
 
+def get_dates_from_url(url):
+    def page_open_body(name):
+        resp = req.get(name)
+        soup = BeautifulSoup(resp.text, 'lxml')
+        main_elem = str(soup.body)
+        return main_elem
 
-def page_open_body(name):
-    resp = req.get(name)
-    soup = BeautifulSoup(resp.text, 'lxml')
-    main_elem = str(soup.body)
-    return main_elem
+    def get_dates(page, pattern):
+        new = re.findall(pattern, str(page))
 
 
-wd_url = []
+        date = ''
+        date_list = []
+        state = False
+        for j in new:
+            for i in j:
+                if i == '<':
+                    state = True
+                if i == '>':
+                    state = False
+                if i == '<':
+                    state = True
+                if i == '>':
+                    state = False
+                if i == '[':
+                    state = True
+                if i == ']':
+                    state = False
+                if not state:
+                    if i != ']' and i != '>':
+                        date += i
+                        temp = i
+            date_list.append(date)
+            date = ''
 
-main_elem = page_open_body("https://en.wikipedia.org/wiki/Thomas_Jefferson")
+        data_list_new = []
 
-# Beautiful Soup
-soup = BeautifulSoup(main_elem)
-film_list = soup.find('table', {'class': 'infobox vcard'})
-# pattern = re.compile(r'eign</th><td>(.*?)</td>')
+        for i in date_list:
+            new_elem = i.replace(u'\xa0', u' ')
+            data_list_new.append(new_elem)
 
-# pattern = re.compile(r'<tr>.*?eign(.*?)</tr><tr>')
-pattern = re.compile(r'<tr>.*?office(.*?)</tr><tr>')
-new = re.findall(pattern, str(film_list))
+        return data_list_new
 
-print(new)
 
-date = ''
-date_list = []
-state = False
-for j in new:
-    for i in j:
-        if i == '<':
-            state = True
-        if i == '>':
-            state = False
-        if i == '[':
-            state = True
-        if i == ']':
-            state = False
-        if not state:
-            if i != ']' and i != '>':
-                date += i
-                temp = i
-    date_list.append(date)
-    date = ''
-'''
-data_list_new = []
-new_str = ''
-flag = False
-for i in date_list:
-    for j in i:
-        if not flag:
-            if j.isdigit():
-                temp_type = 'digit'
-            if j.isalpha():
-                temp_type = 'word'
-            temp = j
-            flag = True
-        if j.isdigit():
-            current_type = 'digit'
-        if j.isalpha():
-            current_type = 'word'
-        if current_type == temp_type:
-            new_str += j
-        if j.isdigit():
-            temp_type = 'digit'
-        if j.isalpha():
-            temp_type = 'word'
-print(new_str)
+    wd_url = []
 
-'''
 
-print(date_list)
+
+    main_elem = page_open_body(url)
+
+    soup = BeautifulSoup(main_elem)
+    page = soup.find('table', {'class': 'infobox vcard'})
+
+
+    pattern = re.compile(r'<tr>.*?office(.*?)</tr><tr>')
+
+    data_list = get_dates(page, pattern)
+
+
+    if not data_list:
+        pattern = re.compile(r'<tr>.*?eign(.*?)</tr><tr>')
+        data_list = get_dates(page, pattern)
+
+    return data_list
